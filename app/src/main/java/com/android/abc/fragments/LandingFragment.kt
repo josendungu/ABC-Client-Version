ad9585341cc2423becca31fef88be78b177921db
Maina Josephat <josendungu.jm@gmail.com>
2021-03-20 02:42:44 -0500

    introducing dataStore ro the splashScreen

--- app/src/main/java/com/android/abc/fragments/LandingFragment.kt
+++ app/src/main/java/com/android/abc/fragments/LandingFragment.kt
@@ -6,22 +6,49 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.asLiveData
 import androidx.navigation.fragment.findNavController
 import com.android.abc.R
+import com.android.abc.SplashScreenStateManager
+import com.android.abc.dataStore
+
 
 class LandingFragment : Fragment() {
+
+    lateinit var splashScreenStateManager: SplashScreenStateManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
 
-        Handler().postDelayed({
+        splashScreenStateManager = context?.dataStore?.let { SplashScreenStateManager(it) }!!
 
-            findNavController().navigate(R.id.action_landing_to_clientDetails)
 
-        }, 1000)
+        splashScreenStateManager.state.asLiveData().observe(viewLifecycleOwner, {
+
+            if (it){
+
+                Handler().postDelayed({
+
+                    findNavController().navigate(R.id.action_landing_to_clientDetails)
+
+                }, 1000)
+
+            } else {
+
+                Handler().postDelayed({
+
+                    findNavController().navigate(R.id.action_landing_to_dashboard)
+
+                }, 1000)
+
+
+            }
+
+        })
+
+
         return inflater.inflate(R.layout.fragment_landing, container, false)
 
 
